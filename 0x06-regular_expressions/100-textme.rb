496839b0825f97fa0be5b11d1529cd7e845bb1a2
endritNovaku <ennald@outlook.com>
2022-06-22 05:00:39 -0700

    fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-a = ARGV[0].scan(/(?<=from:)[a-zA-Z0-9:+-]+/).join
-b = ARGV[0].scan(/(?<=to:)[a-zA-Z0-9:+-]+/).join
-c = ARGV[0].scan(/(?<=flags:)[a-zA-Z0-9:+-]+/).join
-d = a + "," + b + "," + c
-puts "#{d}"
+puts ARGV[0].scan(/(?<=from:|to:|flag)[a-zA-Z0-9:+-]+/).join(",")
